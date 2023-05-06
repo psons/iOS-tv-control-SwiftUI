6a7fd649f5539710e575827569e5dc75f19da60c
Paul Sons <psons@psons@comcast.net>
2023-05-06 10:08:16 -0500

    Initial Commit

---
+++ psons-tv-su/psons_tv_suApp.swift
@@ -0,0 +1,17 @@
+//
+//  psons_tv_suApp.swift
+//  psons-tv-su
+//
+//  Created by Paul Sons on 5/6/23.
+//
+
+import SwiftUI
+
+@main
+struct psons_tv_suApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
